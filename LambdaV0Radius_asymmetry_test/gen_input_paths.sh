5dfcbf103cc31e0430a5e80970ac1aa54cd9248e
cmuncinelli <cmunci@hotmail.com>
2025-11-28 17:09:56 -0300

    Adding multi-threaded capabilities to the Derived Data analysis + SSD staging for quicker execution of code

--- LambdaV0Radius_asymmetry_test/gen_input_paths.sh
+++ LambdaV0Radius_asymmetry_test/gen_input_paths.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 MAX_INDEX=$1
-BASE="/storage2/cicero/asymmetric_rapidity_V0tests_LHC23_pass4_Thin_small"
+BASE="file:/storage2/cicero/asymmetric_rapidity_V0tests_LHC23_pass4_Thin_small"
+# Added "file:" as a prefix because O2 was interpreting the input_data.txt list as a list of LFN entries at the GRID!
 
 for i in $(seq 1 $MAX_INDEX); do
     echo "${BASE}/AO2D_${i}.root"
